8d5944fc4c8c6a75502f1c544b0c9eae4637b40a
samuelatuma1 <atumasaake@gmail.com>
2020-10-13 12:49:41 +0100

    Added ListVew displayed in html. maybe added models

--- adoption/models.py
+++ adoption/models.py
@@ -5,13 +5,20 @@ class Shelter(models.Model):
     name = models.CharField(max_length=50, blank=False)
     address = models.TextField(max_length=1000, blank=False)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Breed(models.Model):
     name = models.CharField(max_length=50, blank=False)
     description = models.TextField(max_length=1000, blank=False)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Pet(models.Model):
     name = models.CharField(max_length=50, blank=False)
     breed = models.ManyToManyField(Breed, related_name='pets')
     shelter = models.ForeignKey(Shelter, on_delete=models.PROTECT, related_name='shelteredpets')
+    def __str__(self):
+        return f'{self.name}'
--- adoption/views.py
+++ adoption/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
 
 # Create your views here.
+
+class IndexView(ListView):
+    context_object_name = 'pets'
+    def get_queryset(self):
+        return models.Pet.objects.all()
+
